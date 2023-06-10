eb2d778bbd29b7569262762b0cc8368be10cf015
Genevive Sanchez <132710480+nevisz@users.noreply.github.com>
2023-06-10 19:55:25 -0400

    Update Main_.java

--- Main_.java
+++ Main_.java
@@ -1,19 +1,20 @@
 import javax.swing.JOptionPane;
 import java.io.IOException;
 
+
 public class Main_ {
     public static void main(String[] args) {
     
+        while (true) {
+
         boolean choiceIsValid = false;
         int playAgain = -1;
-
-        while (true) {
             
             try {
                 Board board = new Board();
-                System.out.println("Word: " + board.getWord().toUpperCase() + "\n");
+                System.out.println("Word: " + board.getWord().toUpperCase() + "\n"); // can comment this out
 
-                while (!board.isDone()) {
+                while(!board.isDone()) {
                     // v strange...it only exits the while loop when there's print or println
                     // otherwise it remains stuck in the loop, even if the condition is false
                     System.out.print("");
